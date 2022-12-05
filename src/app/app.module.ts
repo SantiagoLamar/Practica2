c157dd12c5b44251de3e92d1758adce10273658b
Santiago <justin08lamar@gmail.com>
2022-12-05 11:36:30 -0500

    Primer comnmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentComponent } from './componentes/component/component.component';
+import { Component1Component } from './componentes/component1/component1.component';
+import { Component2Component } from './componentes/component2/component2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentComponent,
+    Component1Component,
+    Component2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
